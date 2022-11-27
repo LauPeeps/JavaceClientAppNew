98f9b8e768bd05edb1b0e138336244a1a377220b
LauPeeps <pilapil.203348@gensan.sti.edu.ph>
2022-11-27 20:14:01 +0800

    new client

--- app/src/main/java/com/example/javaceclientapp/Module.java
+++ app/src/main/java/com/example/javaceclientapp/Module.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -92,5 +93,13 @@ public class Module extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
