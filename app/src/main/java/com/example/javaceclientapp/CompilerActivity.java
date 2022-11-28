1eef4d4f95ef782a5d02da712353ff3cf7f8eb1a
LauPeeps <pilapil.203348@gensan.sti.edu.ph>
2022-11-28 10:45:43 +0800

    new client

--- app/src/main/java/com/example/javaceclientapp/CompilerActivity.java
+++ app/src/main/java/com/example/javaceclientapp/CompilerActivity.java
@@ -3,12 +3,32 @@ package com.example.javaceclientapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class CompilerActivity extends AppCompatActivity {
 
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compiler);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new Callback());
+        webView.loadUrl("https://www.tutorialspoint.com/online_java_compiler.php");
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
     }
 }
